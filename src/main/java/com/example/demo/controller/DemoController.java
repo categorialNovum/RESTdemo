36ec9c9b8ae5e2d7d9c02f1e718f804c10d3f08a
categorialNovum <beaumontjs@gmail.com>
2025-02-28 12:19:55 -0500

    functionality to add cards, retrieve by ID

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,18 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.MagicCard;
+import com.example.demo.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class DemoController {
-
     private final String HELLO_MSG = "{\"message\" : \"hello, world\"}";
     private final String LOREM_MSG = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
             "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
@@ -13,6 +20,13 @@ public class DemoController {
             "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
             "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
             "culpa qui officia deserunt mollit anim id est laborum.";
+
+    private final String CARD_NAME = "Rampant Growth";
+    private final String CARD_TEXT = "Search your library for a basic land card, put that card onto the battlefield tapped, then shuffle.";
+    private final String MANA_COST = "1C,1G";
+
+    @Autowired
+    CardRepository cardRepository;
 
     @GetMapping("/hello")
     String helloWorld() {
@@ -22,5 +36,18 @@ public class DemoController {
     @GetMapping("/lorem")
     String loremIpsum() {
         return LOREM_MSG;
+    }
+
+    @PutMapping("/card/add")
+    MagicCard addCard(){
+        MagicCard card = new MagicCard(CARD_NAME, CARD_TEXT, MANA_COST);
+        System.out.println(card.toString());
+        cardRepository.save(card);
+        return card;
+    }
+
+    @GetMapping("/card/get")
+    Optional<MagicCard> getCard(@RequestParam String id) {
+        return cardRepository.findById(UUID.fromString(id));
     }
 }
--- src/main/java/com/example/demo/entity/MagicCard.java
+++ src/main/java/com/example/demo/entity/MagicCard.java
@@ -17,4 +17,56 @@ public class MagicCard {
     //private ManaCost manaCost;
     private String manaCost;
 //    private CardColors[] colors;
+
+    public MagicCard(){
+        this.cardId = UUID.randomUUID();
+    }
+
+    public MagicCard(String name, String text, String manaCost) {
+        this.name = name;
+        this.cardText = text;
+        this.manaCost = manaCost;
+    }
+
+    public UUID getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(UUID cardId) {
+        this.cardId = cardId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardText() {
+        return cardText;
+    }
+
+    public void setCardText(String cardText) {
+        this.cardText = cardText;
+    }
+
+    public String getManaCost() {
+        return manaCost;
+    }
+
+    public void setManaCost(String manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    @Override
+    public String toString() {
+        return "MagicCard{" +
+                "cardId=" + cardId +
+                ", name='" + name + '\'' +
+                ", cardText='" + cardText + '\'' +
+                ", manaCost='" + manaCost + '\'' +
+                '}';
+    }
 }
