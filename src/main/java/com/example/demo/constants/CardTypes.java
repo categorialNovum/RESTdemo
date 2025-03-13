b434c9550e4245d4c84f9e6f8fadbe88eaeb68a4
categorialNovum <beaumontjs@gmail.com>
2025-03-13 12:50:58 -0400

    add converted mana cost and color extraction to card constructor

---
+++ src/main/java/com/example/demo/constants/CardTypes.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+public enum CardTypes {
+    LAND,
+    ARTIFACT,
+    CREATURE,
+    ENCHANTMENT,
+    INSTANT,
+    SCORCERY,
+    PLANESWALKER,
+    BATTLE,
+    TOKEN
+}
---
+++ src/main/java/com/example/demo/constants/MtgConstants.java
@@ -0,0 +1,15 @@
+package com.example.demo.constants;
+
+public class MagicConstants {
+    public static final String HELLO_MSG = "{\"message\" : \"hello, world\"}";
+    public static final String LOREM_MSG = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim " +
+            "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+            "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
+            "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+            "culpa qui officia deserunt mollit anim id est laborum.";
+
+    public static final String CARD_NAME = "Rampant Growth";
+    public static final String CARD_TEXT = "Search your library for a basic land card, put that card onto the battlefield tapped, then shuffle.";
+    public static final String MANA_COST = "1C,1G";
+}
